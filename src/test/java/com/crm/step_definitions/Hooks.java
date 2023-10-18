790e7ec361d61901184a78f2e45852a2b95c6179
Talha Özbabuccu <talhaoz105@gmail.com>
2023-10-18 11:54:50 +0300

    Hooks.java

--- src/test/java/com/crm/step_definitions/Hooks.java
+++ src/test/java/com/crm/step_definitions/Hooks.java
@@ -1,14 +1,30 @@
 package com.crm.step_definitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
-/*In the class we will be able to pass pre & post conditions to
-each scenario and each step*/
 public class Hooks {
+
     //import from io.cucumber.java not from junit
     @Before
     public void setupScenario(){
         System.out.println("====Setting up browser using cucumber @Before");
     }
 
+    @After
+    public void teardownScenario(){
+        System.out.println("====Closing browser using cucumber @After");
+        System.out.println("====Scenario ended take screenshot if failed!");
+    }
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("---------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("---------> applying tearDown using @AfterStep");
+    }
 }
