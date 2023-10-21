34d8ac3e0ed9d5414dd1a2736736635f250bacda
Talha Özbabuccu <talhaoz105@gmail.com>
2023-10-22 00:13:33 +0300

    Hooks.java take screen shot

--- src/test/java/com/crm/step_definitions/Hooks.java
+++ src/test/java/com/crm/step_definitions/Hooks.java
@@ -1,10 +1,7 @@
 package com.crm.step_definitions;
 
 import com.crm.utilies.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -27,9 +24,11 @@ public class Hooks {
     }
 
     @After
-    public void teardownScenario(){
-
-        byte []screenshots=((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+    public void teardownScenario(Scenario scenario){
+        if (scenario.isFailed()) {
+            byte []screenshot=((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+        }
 
         Driver.closeDriver();
         //System.out.println("====Closing browser using cucumber @After");
