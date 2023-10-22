ccc2f3ac596a4fa5efe45f0943c48ead3a43bd40
Talha Özbabuccu <talhaoz105@gmail.com>
2023-10-22 23:46:51 +0300

    WikiSearchPage.java

--- src/test/java/com/crm/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/crm/step_definitions/Wiki_StepDefinitions.java
@@ -1,24 +1,37 @@
 package com.crm.step_definitions;
 
+import com.crm.pages.WikiSearchPage;
 import com.crm.utilies.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class Wiki_StepDefinitions {
+
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
+
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
         Driver.getDriver().get("https://www.wikipedia.org");
     }
-    @When("User types {Steve Jobs} in the wiki search box")
-    public void user_types_steve_jobs_in_the_wiki_search_box() {
+    @When("User types {String} in the wiki search box")
+    public void user_types_steve_jobs_in_the_wiki_search_box(String string) {
+
+        wikiSearchPage.searchBox.sendKeys(string);
 
     }
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
+
+        wikiSearchPage.searchButton.click();
+
     }
-    @Then("User sees Steve Jobs is in the wiki title")
-    public void user_sees_steve_jobs_is_in_the_wiki_title() {
+    @Then("User sees {String} is in the wiki title")
+    public void user_sees_steve_jobs_is_in_the_wiki_title(String string) {
+        //verify actual title contains string (coming from feature file)
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
     }
 
 }
