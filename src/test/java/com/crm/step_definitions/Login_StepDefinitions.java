5044daa9e21bb3e22d057bd57d41c097825b318b
Talha Özbabuccu <talhaoz105@gmail.com>
2023-12-18 19:39:04 +0300

    Driver.java with ThreadLocal

--- src/test/java/com/crm/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/crm/step_definitions/Login_StepDefinitions.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Login_StepDefinitions {
+
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
         System.out.println("user enters librarian username");
